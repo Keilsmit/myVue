70e070fcb8cccb883e14c03caceecb27e604ff42
keilsmit <keilsmit@yahoo.com>
2017-08-24 16:01:36 -0400

    added methods  to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-# 
-#   helper_method :current_user
 #
-# private
+  helper_method :current_user
 #
-#   def current_user
-#     @user ||= User.find(api_token[:user_id]) if api_token[:user_id]
-#   end
+private
 #
+  def current_user
+    @user ||= User.find_by(api_token: params[:api_token])
+  end
+
 #
-#   def logged_in?
-#     current_user
-#   end
-#
-#   def logged_out?
-#     !current_user
-#   end
+  def logged_in?
+    current_user
+  end
+
+  def logged_out?
+    !current_user
+  end
+
+  def require_login
+    if logged_out?
+      render json: ["Please log in"], status: :forbidden
+    end
+  end
+
+
+
 
 end
